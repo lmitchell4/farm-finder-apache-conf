27e9b14f9b9de67a2e94a6fa8d10cf51b010f1d3
Ubuntu <ubuntu@ip-172-26-1-113.us-west-2.compute.internal>
2017-07-09 16:03:02 +0000

    Add placeholder config file

---
+++ config_placeholder.py
@@ -0,0 +1,9 @@
+APPLICATION_NAME = 'Farm Finder Application'
+APPLICATION_ROOT = '/var/www/html/itemcatalog_venv/itemcatalog/itemcatalog'
+SECRET_KEY = 'secret_key'
+CLIENT_SECRETS_FILE = '/var/www/html/itemcatalog_venv/client_secrets.json'
+UPLOAD_FOLDER_PROFILE = './itemcatalog/static/userImages/profile'
+UPLOAD_FOLDER_ITEM = './itemcatalog/static/userImages/item'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+DATABASE_URI = 'postgresql://username:password@localhost/farmfinder'
+
