3b953c59d6910dc1a906f8421c8a6eeaf876fc8e
lmitchell4 <lmitchell4@gmail.com>
2017-07-11 04:03:11 +0000

    Insert placeholder paths

--- config_placeholder.py
+++ config_placeholder.py
@@ -1,9 +1,10 @@
+
 APPLICATION_NAME = 'Farm Finder Application'
-APPLICATION_ROOT = '/var/www/html/itemcatalog_venv/itemcatalog/itemcatalog'
-SECRET_KEY = 'secret_key'
-CLIENT_SECRETS_FILE = '/var/www/html/itemcatalog_venv/client_secrets.json'
-UPLOAD_FOLDER_PROFILE = './itemcatalog/static/userImages/profile'
-UPLOAD_FOLDER_ITEM = './itemcatalog/static/userImages/item'
+SECRET_KEY = 'secret key'
+CLIENT_SECRETS_FILE = 'path_to_client_secrets_file'
+UPLOAD_FOLDER_PROFILE = 'path_to_directory_for_user_profile_images'
+UPLOAD_FOLDER_ITEM = 'path_to_directory_for_catalogitem_images'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+DEBUG = False
 DATABASE_URI = 'postgresql://username:password@localhost/farmfinder'
 
